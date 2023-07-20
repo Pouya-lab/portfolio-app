3f8b73500a10b084b75e61ca65b7a0e07646541c
pouya <pouyabh102@gmail.com>
2023-07-20 12:38:00 +0330

    navbar completed

---
+++ components/navbar/page.js
@@ -0,0 +1,36 @@
+"use client"
+import { useState } from 'react'
+import './navbar.css'
+import Link from 'next/link'
+import { FaBars , FaTimes } from 'react-icons/fa'
+
+function Navbar() {
+
+    const [ show , setShow ] = useState()
+
+    const switchBurger = ()=>{
+        setShow(!show)
+    }
+
+  return (
+    <>
+        <nav>
+                <h3>Pouya<br/>Behrooj</h3>
+                <div className="">
+                    {/* logic for showing by clicking */}
+                    <ul id="navbar" className={ show ? "#navbar active" : "#navbar"} >
+                            <li><Link className="active" href='#' >Home</Link></li>
+                            <li><Link href='#' >About</Link></li>
+                            <li><Link href='#' >Contact</Link></li>
+                           <li> <Link href='#' >Skills</Link></li>
+                    </ul>
+                </div>
+                <div id="mobile" onClick={switchBurger} >                    
+                        { !show ? <FaBars/> : <FaTimes/> }
+                </div>
+            </nav>
+    </>
+  )
+}
+
+export default Navbar
